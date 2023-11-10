80c7bc4ab52d806765b75c906ba14791581633d1
psy7604 <psyduc_k@163.com>
2023-11-11 00:10:13 +0800

    实现创建和加入房间功能，并分配角色

---
+++ static/js/Page2Component.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>这是页面2的内容</h1>
+      <!-- 其他页面元素 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Vue组件的逻辑部分
+  }
+  </script>
+  
+  <style scoped>
+  /* 页面的样式，scoped属性确保样式只适用于本组件 */
+  </style>
+  
